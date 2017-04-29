46f1f1087b8150d1509df21fd4a27e21d1021155
tniinisto <tniinisto@gmail.com>
2017-04-29 16:38:19 +0300

    test

--- www/js/login.js
+++ www/js/login.js
@@ -22,21 +22,12 @@ function getLogin() {
 
 	$.getJSON(serviceURL + 'login.php', function(data) {
 		$('#busy').hide();
-		//$('#employeeList li').remove();
+
 		logininfo = data.items;
 		 $.each(logininfo, function(index, logininfo) {
 			$('#logininfo').append(logininfo.playerID + ' ' + logininfo.name)
 		 });
 		
-		// 	$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
-		// 			'<img src="pics/' + employee.picture + '" class="list-icon"/>' +
-		// 			'<p class="line1">' + employee.firstName + ' ' + employee.lastName + '</p>' +
-		// 			'<p class="line2">' + employee.title + '</p>' +
-		// 			'<span class="bubble">' + employee.reportCount + '</span></a></li>');
-		// });
-
-		logininfo = data.items;
-
 		setTimeout(function(){
 			scroll.refresh();
 		});
