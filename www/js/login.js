855187d2f0629a7c87e137006593852f7a091d28
tniinisto <tniinisto@gmail.com>
2017-05-19 18:26:01 +0300

    back to test

--- www/js/login.js
+++ www/js/login.js
@@ -8,42 +8,74 @@ var serviceURL = "https://dev-areyouin.azurewebsites.net/pgmobile/services/";
 var logininfo;
 
 $(window).load(function() {
-	setTimeout(getLogin, 100);
+ 	setTimeout(getLogin, 100);
 });
 
 $(document).ajaxError(function(event, request, settings) {
-	$('#busy').hide();
+	//$('#busy').hide();
 	alert("Error accessing the server");
 });
 
-// function getLogin() {
-// 	$('#busy').show();
+ function getLogin() {
+ 	//$('#busy').show();
 
+	$.getJSON(serviceURL + 'login.php', function(data) {
+		$('#busy').hide();
 
-// 	$.getJSON(serviceURL + 'login.php', function(data) {
-// 		$('#busy').hide();
-
-// 		logininfo = data.items;
-// 		$('#logininfo').text(logininfo[0].playerID + ' ' + logininfo[0].name);
+		logininfo = data.items;
+		$('#logininfo').text(logininfo[0].playerID + ' ' + logininfo[0].name);
 		
-// 		// setTimeout(function(){
-// 		// 	scroll.refresh();
-// 		// });
-// 	});
-// }
+		// setTimeout(function(){
+		// 	scroll.refresh();
+		// });
+	});
+}
 
-$("#loginbutton").click(function(){    
+// $("#loginbutton").click(function(e){    
+// 	e.preventDefault();
+	
+// 	// $.ajax({
+//     //     type: 'POST',
+// 	// 	url: 'https://dev-areyouin.azurewebsites.net/pgmobile/services/login.php',
+// 	// 	data: ( {ayiloginname: $("#ayiloginid").value} & {ayipassword: $("#ayipassword").value} ),		
+// 	// 	success: function(data){
+
+// 	// 		// Replace the content of the clicked paragraph with the result from the ajax call
+// 	// 		$("#login_content").html(data);
+// 	// 	}
+// 	// });        
+
+// var name = $("#ayiloginid").value;
+// var password = $("#ayipassword").value;
+
+// 	$.ajax({
+//         type: 'POST',
+// 		url: 'https://dev-areyouin.azurewebsites.net/pgmobile/services/login.php',
+// 		data: { ayiloginname: name, ayipassword: password },		
+// 		success: function(data){
+
+// 			// Replace the content of the clicked paragraph with the result from the ajax call
+// 			$("#login_content").html(data);
+
+// 		}
+// 	});        		
+// });
+
+/*
+$(function() { // equivalent of "$(document).ready(function(){"
+    $('body').on('submit', '#loginform', function(event) {
+        event.preventDefault();
+
+        var formData = $(this).serialize();
+
         $.ajax({
-            url: serviceURL + 'login.php',
-            type: "POST",
-            data: ({ayiloginid: $("#ayiloginid").text}, {ayipassword: $("#ayipassword").text} ),
-            success: function(data){
-                // Why were you reloading the page? This is probably your bug
-                // location.reload();
-
-                // Replace the content of the clicked paragraph
-                // with the result from the ajax call
-                $("#login_content").html(data);
+            type : 'POST',
+            url : 'https://dev-areyouin.azurewebsites.net/pgmobile/services/login.php',
+            data : formData,
+            success : function(data) {
+                $('#login_content').text(data);
             }
-        });        
+        });
     });
+});
+*/
