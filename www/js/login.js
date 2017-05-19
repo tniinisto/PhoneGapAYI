aee3feb642e059d7ab7f5d3a109ef3cf571ec42a
tniinisto <tniinisto@gmail.com>
2017-05-19 14:17:37 +0300

    login parameters with jquery ajax

--- www/js/login.js
+++ www/js/login.js
@@ -16,18 +16,34 @@ $(document).ajaxError(function(event, request, settings) {
 	alert("Error accessing the server");
 });
 
-function getLogin() {
-	$('#busy').show();
+// function getLogin() {
+// 	$('#busy').show();
 
 
-	$.getJSON(serviceURL + 'login.php', function(data) {
-		$('#busy').hide();
+// 	$.getJSON(serviceURL + 'login.php', function(data) {
+// 		$('#busy').hide();
 
-		logininfo = data.items;
-		$('#logininfo').text(logininfo[0].playerID + ' ' + logininfo[0].name);
+// 		logininfo = data.items;
+// 		$('#logininfo').text(logininfo[0].playerID + ' ' + logininfo[0].name);
 		
-		// setTimeout(function(){
-		// 	scroll.refresh();
-		// });
-	});
-}
+// 		// setTimeout(function(){
+// 		// 	scroll.refresh();
+// 		// });
+// 	});
+// }
+
+$("#loginbutton").click(function(){    
+        $.ajax({
+            url: serviceURL + 'login.php',
+            type: "POST",
+            data: ({ayiloginid: $("#ayiloginid").text}, {ayipassword: $("#ayipassword").text} ),
+            success: function(data){
+                // Why were you reloading the page? This is probably your bug
+                // location.reload();
+
+                // Replace the content of the clicked paragraph
+                // with the result from the ajax call
+                $("#login_content").html(data);
+            }
+        });        
+    });
