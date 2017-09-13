63cca182bf4cc64c2648d56b5458f0714dfdaa98
tniinisto@gmail.com <tniinsto@gmail.com>
2017-09-13 18:35:29 +0300

    test2

--- www/js/notification.js
+++ www/js/notification.js
@@ -29,18 +29,26 @@
 
 // }, false);
 
-if (“Notification” in window) {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let’s create a notification
-      if (permission === ‘granted’) {
-        var notification = new Notification(“My title”, {
-             tag: ‘message1’, 
-             body: “My body” 
-        }); 
-        notification.onshow  = function() { console.log(‘show’); };
-        notification.onclose = function() { console.log(‘close’); };
-        notification.onclick = function() { console.log(‘click’); };
-      }
-    });
-  }
-  
+
+$(window).load(function() {
+
+    if ('Notification' in window) {
+        Notification.requestPermission(function (permission) {
+
+        // If the user accepts, let’s create a notification
+        if (permission === ‘granted’) {
+            var notification = new Notification('My title', {
+                tag: ‘message1’, 
+                body: “My body” 
+            }); 
+
+            notification.onshow  = function() { console.log(‘show’); };
+            notification.onclose = function() { console.log(‘close’); };
+            notification.onclick = function() { console.log(‘click’); };
+            
+        }
+        
+        });
+    }
+
+});
