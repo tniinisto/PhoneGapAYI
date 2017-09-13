7a81de3d7a8409e1648baaf320db7f0851726711
tniinisto@gmail.com <tniinsto@gmail.com>
2017-09-13 18:53:14 +0300

    test3

--- platforms/browser/www/js/notification.js
+++ platforms/browser/www/js/notification.js
@@ -29,31 +29,31 @@
 
 // }, false);
 
-document.addEventListener('deviceready', function () {
+//document.addEventListener('deviceready', function () {
 
     $(window).load(function() {
 
         if ('Notification' in window) {
             Notification.requestPermission(function (permission) {
 
-            console.warn(`Notif`);
+            //console.warn(`Notif`);
 
-            // If the user accepts, let’s create a notification
-            // if (permission === ‘granted’) {
-            //     var notification = new Notification('My title', {
-            //         tag: ‘message1’, 
-            //         body: “My body” 
-            //     }); 
+            //If the user accepts, let’s create a notification
+            if (permission === "granted") {
+                var notification = new Notification('My title', {
+                    tag: 'message1', 
+                    body: 'My body' 
+                }); 
 
-            //     notification.onshow  = function() { console.log(‘show’); };
-            //     notification.onclose = function() { console.log(‘close’); };
-            //     notification.onclick = function() { console.log(‘click’); };
+                notification.onshow  = function() { console.warn('show'); };
+                notification.onclose = function() { console.log('close'); };
+                notification.onclick = function() { console.log('click'); };
                 
-            // }
+            }
 
             });
         }
 
     });
 
-});
+//});
