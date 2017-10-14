1ffd87ab47e379f94ab07661191608e66b4bd007
tniinisto@gmail.com <tniinsto@gmail.com>
2017-10-14 10:49:29 +0300

    comments styling

--- www/js/comments.js
+++ www/js/comments.js
@@ -16,28 +16,40 @@ function getComments() {
             //Clear #areyouin-chat-page div contents
             $("comments").html("");
 
-            $("#areyouin-chat-page" ).append("<div id='comments' style='padding: 10px;'> <ul data-role='listview' data-inset='true'>");
+            $("#areyouin-chat-page" ).append("<div id='comments' style='padding: 10px;'>").enhanceWithin();
 
             $.each(result, function (key, data) {
                
                 $.each(data, function (index, data) {
 
-                //var i = 0;
+                var i = 0;
+
                 //while(i < 20) {                
 
                 //$.each(result, function (key, data) {
 
                     //var photo = result.items[i].photourl;
-
-                    $("#areyouin-chat-page" ).append(
-                        "<li>"
-                            + "<img src='https://r-youin.com/images/" + data.photourl + "'>"
+                if(i == 0) {
+                    $("#comments" ).append(
+                        "<ul data-role='listview' data-inset='true'>"
+                        +"<li style='margin: 2px; background: #39414b'>"
+                            + "<img style='margin: 8px;' src='https://r-youin.com/images/" + data.photourl + "'>"
                             + "<h2>Broken Bells</h2>"
                             + "<p>Broken Bells</p></a>"
                         + "</li>"
-                    )
+                    ).enhanceWithin();
+                } 
+                else {
+                    $("#comments" ).append(
+                        "<li style='margin: 2px; background: #39414b'>"
+                            + "<img style='margin: 8px;' src='https://r-youin.com/images/" + data.photourl + "'>"
+                            + "<h2>Broken Bells</h2>"
+                            + "<p>Broken Bells</p></a>"
+                        + "</li>"
+                    ).enhanceWithin();
+                }
 
-                    //i++;                    
+                i++;                    
 
                 });
             });
