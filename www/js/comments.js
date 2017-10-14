b603cc0653af8e459936289cd8083c48117e0cf6
tniinisto@gmail.com <tniinsto@gmail.com>
2017-10-14 10:28:18 +0300

    comments with foreach solved

--- www/js/comments.js
+++ www/js/comments.js
@@ -18,25 +18,29 @@ function getComments() {
 
             $("#areyouin-chat-page" ).append("<div id='comments' style='padding: 10px;'> <ul data-role='listview' data-inset='true'>");
 
-                var i = 0;
-                while(i < 20) {                
+            $.each(result, function (key, data) {
+               
+                $.each(data, function (index, data) {
+
+                //var i = 0;
+                //while(i < 20) {                
 
                 //$.each(result, function (key, data) {
 
-                    // var photo = 'player11.png';
-                    var photo = result.items[i].photourl;
+                    //var photo = result.items[i].photourl;
 
                     $("#areyouin-chat-page" ).append(
                         "<li>"
-                            + "<img src='https://r-youin.com/images/" + photo + "'>"
+                            + "<img src='https://r-youin.com/images/" + data.photourl + "'>"
                             + "<h2>Broken Bells</h2>"
                             + "<p>Broken Bells</p></a>"
                         + "</li>"
                     )
 
-                    i++;                    
+                    //i++;                    
 
-                }
+                });
+            });
             
             $("#areyouin-chat-page" ).append("</ul></div>").enhanceWithin();
 
