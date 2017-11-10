a2bb1f3c02fe035553614161770c5489aa8030aa
tniinisto@gmail.com <tniinsto@gmail.com>
2017-11-10 13:50:20 +0200

    sso with get

--- platforms/browser/www/js/sso.js
+++ platforms/browser/www/js/sso.js
@@ -1,14 +1,17 @@
 // var serviceURL = "https://r-youin.com/pgmobile/services/";
 
-$('#sso_form').on('submit', function () {
-    //alert("sso start");
+// $('#sso_form').on('submit', function () {
+//     //alert("sso start");
 
-    //create AJAX request to submit form
-    $.GET(this.action, $(this).serialize(), function (response) {
-        //the `response` variable now holds what the server returned after the form submission
+//     window.open('https://r-youin.com/logincheck.php', '_blank', 'location=yes');
+
+//     //create AJAX request to submit form
+//     // $.GET(this.action, $(this).serialize(), function (response) {
+//     //     //the `response` variable now holds what the server returned after the form submission
         
-    });
+//     // });
 
-    //stop regular submission of form
-    return false;
-});
+//     //stop regular submission of form
+//     return false;
+    
+// });
