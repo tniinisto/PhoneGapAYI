2ab22bf2f8351138903651261dbfdc4a06577afc
tniinisto@gmail.com <tniinsto@gmail.com>
2017-11-16 17:23:31 +0200

    color

--- www/js/events.js
+++ www/js/events.js
@@ -135,7 +135,7 @@ function getEvents(teamid, afterlogin=0) {
                         "<div style='text-align:center; padding-bottom: 2em; margin-top: 1em; background: #39414b; height: auto; width: auto; padding-top: 1em;'>"
                         
                             + "<h1 style='font-size: 110%; font-weight: bold; color: lightgrey;' id='eventstatus_0' style='background: #39414b; margin-top: -15px;'>No events scheduled at the moment...</h1>"
-                            + "<div style='padding-left: 5px; padding-right: 5px;'><button><a style='text-transform: none; text-decoration: underline; font-size: 100%; color: lightgrey;' onclick='loadWebURL();'>Schedule a new event from web</a></button></div>"
+                            + "<div style='padding-left: 5px; padding-right: 5px;'><button><a style='text-transform: none; text-decoration: underline; font-size: 100%; color: #2ad;' onclick='loadWebURL();'>Schedule a new event from web</a></button></div>"
                                                         
                         + "</div>"  
                     
