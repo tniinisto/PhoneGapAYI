20fedb4052eb8b9e0f38087d330ec946dd3933c3
tniinisto@gmail.com <tniinsto@gmail.com>
2017-11-18 09:02:52 +0200

    fix for private event showing

--- platforms/browser/www/js/events.js
+++ platforms/browser/www/js/events.js
@@ -166,7 +166,7 @@ function getEvents(teamid, afterlogin=0) {
                         private_info = '';
 
                     //Show private event only for invited users => 0 = public event, 2 = private event and user is invited, show always for admins and registrar
-                    if(areyouin[i][7] == 0 || areyouin[i][7] == 2 || areyouin[i][6] != 0 || sessionStorage['admin'] == 1 || sessionStorage['registrar'] == 1) {
+                    if(areyouin[i][7] == 0 || areyouin[i][7] == 2 || sessionStorage['admin'] == 1 || sessionStorage['registrar'] == 1) {
 
                         //List all event participants, except the logged in user
                         invited += areyouin[i][2];
